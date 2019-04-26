21db13c2854b895db812480d6845f59c9a59c92c
thomasvineberg <47236191+thomasvineberg@users.noreply.github.com>
2019-04-25 21:01:44 -0700

    Add files via upload

---
+++ Vineberg_thomas_assignment9/part3/team.js
@@ -0,0 +1,28 @@
+{"teammembers":[
+        {
+            "name":"Agnes",
+            "title":"Vice President of Accounting",
+            "age":"24"
+        },
+        {
+            "name":"Astrid",
+            "title":"Vice President of Sales",
+            "age":"34"
+        },
+        {
+            "name":"Amy",
+            "title":"Vice President of Technology",
+            "age":"44"
+        },
+        {
+            "name":"Amanda",
+            "title":"Vice President of Human Resources",
+            "age":"54"
+        },
+        {
+            "name":"Annabelle",
+            "title":"Vice President of Compliance",
+            "age":"64"
+        }
+    
+]}
